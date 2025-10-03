c4998865b91a19f6df15c4f80516383c3e763fed
Steve Bilogan <steve.bilogan@gmail.com>
2025-10-02 23:51:48 -0400

    chore: move to maui font builder

--- DailyReflection/MauiProgram.cs
+++ DailyReflection/MauiProgram.cs
@@ -30,6 +30,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("FontAwesomeBrands.otf", "FaBrandsFont");
+				fonts.AddFont("FontAwesomeRegular.otf", "FaRegularFont");
+				fonts.AddFont("FontAwesomeSolid.otf", "FaSolidFont");
 			});
 
 		builder.Configuration.AddConfiguration(config);
